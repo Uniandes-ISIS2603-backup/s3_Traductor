d84764fe52c1b2331255db18aa9f2cd5d5acc830
Geovanny Andres Gonzalez <ga.gonzalezr@uniandes.edu.co>
2018-08-27 12:56:08 -0500

    Se crea la entidad para Propuesta y se mejora el DTO para su posterior ensamble

--- s3_traductor-api/src/main/java/co/edu/uniandes/csw/traductor/dtos/PropuestaDTO.java
+++ s3_traductor-api/src/main/java/co/edu/uniandes/csw/traductor/dtos/PropuestaDTO.java
@@ -5,6 +5,7 @@
  */
 
 package co.edu.uniandes.csw.traductor.dtos;
+import co.edu.uniandes.csw.traductor.entities.PropuestaEntity;
 import java.io.Serializable;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -26,11 +27,10 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  *
  * </pre>
  *
- * @author Geovanny Andrés Gonzalez
+ * @author Geovanny Andres Gonzalez
  */
 
-
-public class PropuestaDTO {
+public class PropuestaDTO implements Serializable{
     
     private Long propuestaId;
     private String descripcion;
@@ -46,7 +46,51 @@ public class PropuestaDTO {
 	public PropuestaDTO() {
 	}
 
+	//===================================================
+	//Metodos de transformacion
+	//===================================================
+	
+	/**
+     * Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
+     * la entidad que viene de argumento.
+     *
+     * @param propuestaEntity: Es la entidad que se va a convertir a DTO
+     */
+	
+	public PropuestaDTO (PropuestaEntity propuestaEntity)
+	{
+		if (propuestaEntity != null)
+		{
+			this.descripcion = propuestaEntity.getDescripcion();
+			this.costo = propuestaEntity.getCosto();
+			this.estado = propuestaEntity.getEstado();
+			this.idEmpleado = propuestaEntity.getIdEmpleado();
+			this.fecha = propuestaEntity.getFecha();			
+		}
+	}
+	
+	/**
+     * Conviertir DTO a Entity (Crea un nuevo Entity con los valores que posee el DTO).
+	 * @return Una PropuestaEntity con los valores que posee el DTO.
+     */
+	
+	public PropuestaEntity toEntity()
+	{
+		PropuestaEntity propuestaEntity = new PropuestaEntity();
+		propuestaEntity.setCosto(costo);
+		propuestaEntity.setDescripcion(descripcion);
+		propuestaEntity.setEstado(estado);
+		propuestaEntity.setIdEmpleado(idEmpleado);
+		propuestaEntity.setFecha(fecha);
+		return propuestaEntity;
+	}
+	
+	//===================================================
+	//Metodos
+	//===================================================
+	
     /**
+	 * ID de la propuesta.
      * @return the propuestaId
      */
 	
@@ -55,6 +99,7 @@ public class PropuestaDTO {
     }
 
     /**
+	 * Cambia el ID de la propuesta.
      * @param propuestaId the propuestaId to set
      */
     public void setPropuestaId(Long propuestaId) {
@@ -62,72 +107,89 @@ public class PropuestaDTO {
     }
 
     /**
-     * @return the descripcion
-     */
-    public String getDescripcion() {
-        return descripcion;
-    }
+	 * Descripcion de la propuesta
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
 
-    /**
-     * @param descripcion the descripcion to set
-     */
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
+	/**
+	 * Cambia la descripcion de la propuesta
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
-    /**
-     * @return the costo
-     */
-    public String getCosto() {
-        return costo;
-    }
+	/**
+	 * Costo de la realizacion del trabajo
+	 * @return the costo
+	 */
+	
+	public String getCosto() {
+		return costo;
+	}
 
-    /**
-     * @param costo the costo to set
-     */
-    public void setCosto(String costo) {
-        this.costo = costo;
-    }
+	/**
+	 * Cambia el costo del trabajo
+	 * @param costo the costo to set
+	 */
+	
+	public void setCosto(String costo) {
+		this.costo = costo;
+	}
 
-    /**
-     * @return the estado
-     */
-    public String getEstado() {
-        return estado;
-    }
+	/**
+	 * Estado de progreso del trabajo
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
 
-    /**
-     * @param estado the estado to set
-     */
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
+	/**
+	 * Cambia el estado del trabajo a medida que se va progresando
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 
-    /**
-     * @return the idEmpleado
-     */
-    public Long getIdEmpleado() {
-        return idEmpleado;
-    }
+	/**
+	 * Retorna el ID del empleado que va a realizar el trabajo.
+	 * @return the idEmpleado
+	 */
+	public Long getIdEmpleado() {
+		return idEmpleado;
+	}
 
-    /**
-     * @param idEmpleado the idEmpleado to set
-     */
-    public void setIdEmpleado(Long idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
+	/**
+	 * Cambia el ID del empleado que va a realizar el trabajo.
+	 * @param idEmpleado the idEmpleado to set
+	 */
+	public void setIdEmpleado(Long idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
 
-    /**
-     * @return the fecha
-     */
-    public String getFecha() {
-        return fecha;
-    }
+	/**
+	 * Retorna la fecha estimada de entrega
+	 * @return the fecha
+	 */
+	public String getFecha() {
+		return fecha;
+	}
 
-    /**
-     * @param fecha the fecha to set
-     */
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }    
+	/**
+	 * Cambia la fecha estimada de entrega
+	 * @param fecha the fecha to set
+	 */
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}  
+	
+	@Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
