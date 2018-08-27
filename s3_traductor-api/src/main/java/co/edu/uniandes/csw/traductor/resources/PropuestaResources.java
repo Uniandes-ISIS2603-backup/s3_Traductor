87761762ad832758c9ff5191e5695c29be2abd68
Geovanny Andres Gonzalez <ga.gonzalezr@uniandes.edu.co>
2018-08-27 15:09:13 -0500

    Inicio en la creacion de resources

---
+++ s3_traductor-api/src/main/java/co/edu/uniandes/csw/traductor/resources/PropuestaResources.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.traductor.resources;
+import java.util.*;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+
+/**
+ * Clase que implementa el recurso invitacion
+ * @author Geovanny Andres Gonzalez
+ */
+
+@Path("propuestas")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class PropuestaResources 
+{
+
+	private static final Logger LOG = Logger.getLogger(PropuestaResources.class.getName());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
