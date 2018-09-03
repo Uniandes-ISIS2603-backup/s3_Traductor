2f3ae8944721600e19b456da38d8ba1e75aef76b
salozord <s.salazara@uniandes.edu.co>
2018-09-03 10:47:16 -0500

    Hecho el test de cliente y probado. Falta hacer las demás operaciones

---
+++ s3_traductor-back/src/test/java/co/edu/uniandes/csw/traductor/test/persistance/ClientePersistenceTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.traductor.test.persistance;
+
+import co.edu.uniandes.csw.traductor.entities.ClienteEntity;
+import co.edu.uniandes.csw.traductor.persistance.ClientePersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ * Puebas para Cliente Persistance
+ * @author Santiago Salazar
+ */
+@RunWith(Arquillian.class)
+public class ClientePersistenceTest {
+    /**
+     * Inyeccion de dependencias de la persistencia para las pruebas
+     */
+    @Inject
+    private ClientePersistence clientePersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ClienteEntity.class.getPackage())
+                .addPackage(ClientePersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Test
+    public void createClienteTest()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        ClienteEntity newEntity = factory.manufacturePojo(ClienteEntity.class);
+        ClienteEntity result = clientePersistence.create(newEntity);
+        
+        Assert.assertNotNull(result);
+
+        ClienteEntity entity = em.find(ClienteEntity.class, result.getId());
+
+        assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+}
