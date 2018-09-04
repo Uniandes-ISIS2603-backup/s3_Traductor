5b47870a0a1f0272af2c3bd6e05b926242bdd99e
salozord <s.salazara@uniandes.edu.co>
2018-09-03 22:04:40 -0500

    Finalizado implementacion de un resource relacion. Realizando pruebas pero no sirven por algo del ArquillianServletRunner, Falta resolverlo e implementar los resources faltantes de las relaciones que tiene cliente.

--- s3_traductor-back/src/test/java/co/edu/uniandes/csw/traductor/test/persistance/ClientePersistenceTest.java
+++ s3_traductor-back/src/test/java/co/edu/uniandes/csw/traductor/test/persistance/ClientePersistenceTest.java
@@ -7,15 +7,19 @@ package co.edu.uniandes.csw.traductor.test.persistance;
 
 import co.edu.uniandes.csw.traductor.entities.ClienteEntity;
 import co.edu.uniandes.csw.traductor.persistance.ClientePersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -36,6 +40,11 @@ public class ClientePersistenceTest {
     @PersistenceContext
     private EntityManager em;
     
+    @Inject
+    UserTransaction utx;
+    
+    private List<ClienteEntity> data = new ArrayList<ClienteEntity>();
+    
     /**
      * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
      * El jar contiene las clases, el descriptor de la base de datos y el
@@ -50,6 +59,30 @@ public class ClientePersistenceTest {
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
     
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Prueba para crear un cliente
+     */
     @Test
     public void createClienteTest()
     {
@@ -62,5 +95,28 @@ public class ClientePersistenceTest {
         ClienteEntity entity = em.find(ClienteEntity.class, result.getId());
 
         assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from ClienteEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+
+            ClienteEntity entity = factory.manufacturePojo(ClienteEntity.class);
+
+            em.persist(entity);
+
+            data.add(entity);
+        }
     }
 }
