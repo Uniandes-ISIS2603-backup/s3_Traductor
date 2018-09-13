ec48f2651a637c9c24c31e2775b0fa5a31e042fc
Geovanny Andres Gonzalez <ga.gonzalezr@uniandes.edu.co>
2018-09-13 11:51:31 -0500

    Primer metodo de la logica de Propuesta

--- s3_traductor-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s3_traductor-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -33,6 +33,8 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.traductor.mappers.WebApplicationExceptionMapper.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.AreaConocimientoResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.CalificacionResource.class);
+        resources.add(co.edu.uniandes.csw.traductor.resources.ClienteInvitacionResource.class);
+        resources.add(co.edu.uniandes.csw.traductor.resources.ClientePagosResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.ClienteResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.ClienteSolicitudesResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.ClienteTarjetasResource.class);
@@ -41,8 +43,8 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.traductor.resources.InvitacionResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.PagosResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.PropuestaResource.class);
-        resources.add(co.edu.uniandes.csw.traductor.resources.SolicitudResource.class);
-        resources.add(co.edu.uniandes.csw.traductor.resources.TarjetaDeCreditoResource.class);
+		resources.add(co.edu.uniandes.csw.traductor.resources.SolicitudResource.class);
+		resources.add(co.edu.uniandes.csw.traductor.resources.TarjetaDeCreditoResource.class);
     }
     
 }
--- s3_traductor-back/src/main/java/co/edu/uniandes/csw/traductor/ejb/PropuestaLogic.java
+++ s3_traductor-back/src/main/java/co/edu/uniandes/csw/traductor/ejb/PropuestaLogic.java
@@ -4,11 +4,67 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.traductor.ejb;
+import co.edu.uniandes.csw.traductor.entities.PropuestaEntity;
+import co.edu.uniandes.csw.traductor.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.traductor.persistence.PropuestaPersistence;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 /**
  * Clase que verifica las reglas del negocio de la clase propuesta.
  * @author Geovanny Andres Gonzalez
  */
-public class PropuestaLogic {
+
+@Stateless
+public class PropuestaLogic
+{
+	private static final Logger LOGGER = Logger.getLogger(PropuestaLogic.class.getName());
+	public final static String EN_PROCESO = "EN_PROCESO"; //Valor de estado para la propuesta.
+	public final static String CULMINADA = "CULMINADA"; //Valor de estado para la propuesta.
 	
+	@Inject
+	private PropuestaPersistence propuestaPersistence; //Invocación a la tabla de propuesta para trabajar en la base de datos.
+	
+	/**
+     * Crea una propuesta en la persistencia.
+     *
+     * @param propuestaEntity La entidad que representa la propuesta a
+     * persistir.
+     * @return La entidad de la propuesta luego de persistirla.
+     * @throws BusinessLogicException Si la editorial a persistir ya existe o si no se cumple la integridad de los datos requeridos.
+     */
+	
+	public PropuestaEntity createPropuesta(PropuestaEntity propuestaEntity) throws BusinessLogicException
+	{
+		LOGGER.log(Level.INFO, "[LOGICA] Inicia proceso de creación de la propuesta");
+        
+		// Verifica la regla de negocio. Verificacion de los datos del Entity.
+		
+		if (propuestaEntity.getIdEmpleado() == null)
+		{
+			throw new BusinessLogicException("El id de empleado es un valor nulo"); 
+		}
+		
+		else if (propuestaEntity.getCosto() < 0)
+		{
+			throw new BusinessLogicException("El costo de la propuesta no puede ser un valor negativo, su valor fue: " + propuestaEntity.getCosto()); 
+		}
+        
+		else if (!propuestaEntity.getEstado().equals(CULMINADA) || !propuestaEntity.getEstado().equals(EN_PROCESO))
+		{
+			throw new BusinessLogicException("El estado de la propuesta no es correcto se esperaba EN_PROCESO o CULMINADA y se obtuvo: " + propuestaEntity.getEstado()); 
+		}
+		
+		else if (propuestaEntity.getDescripcion().length() == 0 || propuestaEntity.getDescripcion() == null)
+		{
+			throw new BusinessLogicException("La descripcion de la propuesta no puede ser nula o vacia");
+		}
+		
+        // Invoca la persistencia para crear la propuesta pues se ha validado todas las reglas.
+        propuestaPersistence.create(propuestaEntity);
+        LOGGER.log(Level.INFO, "[LOGICA] Termina proceso de creación de la propuesta");
+        return propuestaEntity;		
+	}
 }
