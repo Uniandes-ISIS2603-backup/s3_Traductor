d144a361114b25a30adc4aa3e60f371ad4aaab5b
JuanParra201714259 <jf.parrac@uniandes.edu.co>
2018-09-19 20:58:30 -0500

    empleado calificacion terminado y creacion logica SolicitudIdioma

---
+++ s3_traductor-back/src/main/java/co/edu/uniandes/csw/traductor/ejb/EmpleadoCalificacionLogic.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.traductor.ejb;
+
+import co.edu.uniandes.csw.traductor.entities.CalificacionEntity;
+import co.edu.uniandes.csw.traductor.entities.EmpleadoEntity;
+import co.edu.uniandes.csw.traductor.persistence.CalificacionPersistence;
+import co.edu.uniandes.csw.traductor.persistence.EmpleadoPersistence;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Juan Felipe Parra
+ */
+public class EmpleadoCalificacionLogic {
+    private final static Logger LOGGER=Logger.getLogger(EmpleadoCalificacionLogic.class.getName());
+    
+    @Inject
+    private EmpleadoPersistence empleadoPersistence;
+    @Inject
+    private CalificacionPersistence calificacionPersistence;
+    /**
+     * agrega una calificacion al empleado indicado por el parametro
+     * @param empleadoId identificador del empleado al cual se le asignara una calificacion
+     * @param calificacionId identificador del id a asignar
+     * @return la entidad encontrada luego de la persistencia
+     */
+    public   CalificacionEntity  addCalificaion(Long empleadoId,Long calificacionId){
+        LOGGER.log(Level.INFO,"Inicia proceso de asocicacion de calificacion a un empleado con id {0}",empleadoId);
+        //EmpleadoEntity entidadSuperior=empleadoPersistence.find(clienteId);
+        CalificacionEntity entidadInferior=calificacionPersistence.find(calificacionId);
+        //entidadSuperior.getCalificaciones().add(entidadInferior);
+        LOGGER.log(Level.INFO,"Termina proceso de agregar una calificacion a un empleado con id {0}",empleadoId);
+        return calificacionPersistence.find(calificacionId);
+    }
+
+}
