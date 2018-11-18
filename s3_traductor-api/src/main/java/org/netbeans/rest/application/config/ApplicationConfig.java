a0e2fce9f836bb1c239ab8499e91db85489ba36c
estudiante <aj.ayte@uniandes.edu.co>
2018-11-17 19:42:57 -0500

    merge

--- s3_traductor-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s3_traductor-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -40,9 +40,9 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.traductor.resources.EmpleadoResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.IdiomaResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.InvitacionResource.class);
-        resources.add(co.edu.uniandes.csw.traductor.resources.SolicitudIdiomaResource.class);
+        resources.add(co.edu.uniandes.csw.traductor.resources.PropuestaResource.class);
         resources.add(co.edu.uniandes.csw.traductor.resources.SolicitudResource.class);
+        resources.add(co.edu.uniandes.csw.traductor.resources.TarjetaDeCreditoResource.class);
 }
     
 }
-
